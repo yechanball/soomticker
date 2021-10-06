463c19307b819dff708bfac817f33b8e4770afef
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-10-06 20:44:34 +0900

    Update main_notutorial.js

--- js/main_notutorial.js
+++ js/main_notutorial.js
@@ -1,5 +1,4 @@
 /* javascript of main.html */
-var agent = navigator.userAgent.toLowerCase();
 
 /* Page loading */
 window.onload = function() {
@@ -98,11 +97,6 @@ swiper.on('progress', function(){
         }
         else if(swiper.progress<0.4){
             /* stop swiping */
-            if(agent.indexOf("safari")!=-1){
-                console.log("It is Safari browser!");
-            }else{
-                window.navigator.vibrate([50]);
-            }
         }
         else{
             document.all.timer.style.opacity = 0;
@@ -123,11 +117,7 @@ swiper.on('slideChange', function(){
 
         document.all.recordfinishpop.style.visibility = "visible";
         popDownElement.classList.add('animate__animated', 'animate__fadeInDown');
-        if(agent.indexOf("safari")!=-1){
-            console.log("It is Safari browser!");
-        }else{
-            window.navigator.vibrate([150]);
-        }
+        window.navigator.vibrate([100]);
     }
 });
 
