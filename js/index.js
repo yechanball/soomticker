88eaf28a4d1b25f60a27792a8f9f265b254994cb
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-11-21 14:27:45 +0900

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 window.onload = function() {
-    const element = document.querySelector('.animation-element');
-    document.all.logoArea.style.visibility="visible";
-    element.classList.add('animate__animated', 'animate__fadeIn', 'animate__slow');
+    setTimeout(function() {
+        const element = document.querySelector('.animation-element');
+        document.all.logoArea.style.visibility="visible";
+        element.classList.add('animate__animated', 'animate__fadeIn', 'animate__slow');
 
-    element.addEventListener('animationend', () => {
-        //////////////////////////////////////////
-        // 쿼리 확인해서 재생 or 튜토리얼로 전환
-        /*
-        if(쿼리 == true){
-            location.replace("./play.html");
-        }else if(쿼리 == false){
+        element.addEventListener('animationend', () => {
+            //////////////////////////////////////////
+            // 쿼리 확인해서 재생 or 튜토리얼로 전환
+            /*
+            if(쿼리 == true){
+                location.replace("./play.html");
+            }else if(쿼리 == false){
+                location.replace("./tutorial.html");
+            }else{
+                // 잘못된 접근
+                location.replace("./contact.html");
+            }
+            */
+            /////////////////////////////////////////
             location.replace("./tutorial.html");
-        }else{
-            // 잘못된 접근
-            location.replace("./contact.html");
-        }
-        */
-        /////////////////////////////////////////
-        location.replace("./tutorial.html");
-    });
+        });
+    }, 1000);
 };
