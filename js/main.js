a4c563d741ac5226c6035ee2493ce08920bde22c
Yechan <77843815+yechan-nfc@users.noreply.github.com>
2022-02-10 03:27:09 +0900

    Edit popup design

--- js/main.js
+++ js/main.js
@@ -129,6 +129,19 @@ swiper.on('slideChange', function () {
     }
 });
 
+/* Upload form function */
+$(document).ready(function () {
+    var fileTarget = $('.fileboxInner .upload-hidden');
+
+    fileTarget.on('change', function () {
+        if (window.FileReader) {
+            var filename = $(this)[0].files[0].name;
+        } else {
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        }
+        $(this).siblings('.upload-name').val(filename);
+    });
+}); 
 
 /* Check upload file type */
 function getExtension(filename) {
